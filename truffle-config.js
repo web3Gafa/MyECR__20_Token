6caec3d4a026243298118b6103b1bc523eb1a9f8
dzzirima@gmail.com <dzzirima@gmail.com>
2021-12-18 19:29:44 +0200

    firnal project

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,4 @@
-require("dotenv").config({path:"./env"})
+require("dotenv").config({path:"./.env"})
 const path = require("path");
 const HDWalletProvider = require("@truffle/hdwallet-provider")
 
@@ -22,6 +22,18 @@ module.exports = {
     //   },
     //   network_id:5777
     // }
+    goerli_infura:{
+      provider:function(){
+        return new HDWalletProvider(process.env.MNEMONIC,"https://goerli.infura.io/v3/60be7748964d45d288da29f2b06ca27f",AccountIndex)
+      },
+      network_id:5
+    },
+    ropsten_infura:{
+      provider:function(){
+        return new HDWalletProvider(process.env.MNEMONIC,"https://ropsten.infura.io/v3/60be7748964d45d288da29f2b06ca27f",AccountIndex)
+      },
+      network_id:3
+    }
   },
   compilers: {
     solc: {
