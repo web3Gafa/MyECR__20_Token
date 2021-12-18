4e23f6ee12f07144972fa21aecb56ef5301e6831
dzzirima@gmail.com <dzzirima@gmail.com>
2021-12-18 10:22:56 +0200

    deployed my token sale papa

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,16 @@
 var MyToken = artifacts.require("MyToken.sol");
+var MyTokenSale = artifacts.require("MyTokenSale")
 
 
 module.exports = async function(deployer){
+
+    let addr = await web3.eth.getAccounts()
+
     await deployer.deploy(MyToken,100000);
+    await deployer.deploy(MyTokenSale,1,addr[0],MyToken.address)
+
+    // sending all the tokens to Token Sale Contract
+    let instance = await MyToken.deployed();
+    await  instance.transfer(MyTokenSale.address,100000)
+
 }
